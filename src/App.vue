0ef2d89e7ed1d3fb83b03ae7121a25e938a7bedb
Candice <candicebranchereau@gmail.com>
2022-02-16 19:13:39 +0100

    test first component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld/>
+    <HelloWorld/>
   </div>
 </template>
 
@@ -24,5 +25,7 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  justify-content: space-between;
 }
 </style>
